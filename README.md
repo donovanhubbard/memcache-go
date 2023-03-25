39f8d284738b139affa7dab3b7bd784bd9351c52
Donovan Hubbard <donovan.hubbard@gmail.com>
2023-03-25 08:57:07 -0500

    Can pass host and port as an environment variable

--- README.md
+++ README.md
@@ -2,18 +2,8 @@
 
 A memcached cli written in go.
 
-## Running
-
-flags
-
--host
--port
-<commands>
-
 ## Todo
 
-- Convert to a real logging framework
-- Accept the host and port through environment variables
 - Validate get command succeeded and get output back to end user
 - Validate program arguments that are specific to each command
 - Make sure the get command writs to STDOUT
--- cmd/cli.go
+++ cmd/cli.go
@@ -22,6 +22,8 @@ func main(){
 	var key string
 	var value string
 
+	var err error
+
 
 	flag.StringVar(&host, "host", "localhost", "hostname or ip address of the memcached server")
 	flag.IntVar(&port,"port",11211,"memcached port. Default: 11211")
@@ -32,6 +34,24 @@ func main(){
 	flag.StringVar(&value, "value", "", "Value to be set")
 
 	flag.Parse()
+
+	if os.Getenv("HOST") != "" {
+		host = os.Getenv("HOST")
+		utils.Sugar.Debugf("Using environment variable HOST to set host=[%s]",host)
+	}
+
+	if os.Getenv("PORT") != "" {
+		port, err = strconv.Atoi(os.Getenv("PORT"))
+		if err != nil {
+			utils.Sugar.Error("Failed converting environment variable PORT to integer. " + err.Error())
+			os.Stderr.WriteString("Invalid port number. Must be an integer")
+			os.Exit(1)
+		}
+		utils.Sugar.Debugf("Using environment variable PORT to set port=[%d]",port)
+	}
+
+	utils.Sugar.Debugf("host=[%s]",host)
+	utils.Sugar.Debugf("port=[%d]",port)
 
 	if host == "" {
 		os.Stderr.WriteString("Missing mandatory argument '-host'\n")
@@ -45,7 +65,6 @@ func main(){
 
 	c := client.Client {Host: host, Port: port}
 
-	var err error 
 	var returnedValue string
 
 	if command == "set" {
